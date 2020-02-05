4a3281d887cae3158a808f8fc3d6da510b347abb
karkic <conneraschell@hotmail.com>
2020-02-05 14:20:44 -0800

    Add files via upload

--- SpongePixelmonTesting/src/main/java/com/pixelmonseasonthree/mcdev/mcdev/Mcdev.java
+++ SpongePixelmonTesting/src/main/java/com/pixelmonseasonthree/mcdev/mcdev/Mcdev.java
@@ -3,11 +3,15 @@ package com.pixelmonseasonthree.mcdev.mcdev;
 import com.google.common.eventbus.Subscribe;
 import com.google.inject.Inject;
 import com.sun.xml.internal.ws.util.xml.ContentHandlerToXMLStreamWriter;
+import org.apache.commons.lang3.event.EventListenerSupport;
 import org.slf4j.Logger;
 import org.spongepowered.api.Server;
+import org.spongepowered.api.Sponge;
 import org.spongepowered.api.command.spec.CommandSpec;
 import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.event.Event;
 import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.command.SendCommandEvent;
 import org.spongepowered.api.event.game.state.GameInitializationEvent;
 import org.spongepowered.api.event.game.state.GameStartedServerEvent;
 
@@ -41,14 +45,27 @@ public class Mcdev {
 
     @Listener
     public void onInitialize( GameInitializationEvent event){
+
+        instance = this;
        logger.info("This is a test");
         CommandSpec command = CommandSpec.builder()
                 .permission("PluginSeason3.command.base")
                 .description(Text.of("This is the base command"))
                 .executor(new SendMessage()).build();
+        
 
 
 
+        Sponge.getCommandManager().register(instance,command, "PixelmonSeasonThree","Season");
 
+    }
+
+    @Listener
+    public void onCommmand(SendCommandEvent event){
+
+        if (event.getCommand().equalsIgnoreCase("say")) {
+            Player player = (Player) event.getSource();
+            event.setCancelled(true);
+        }
     }
 }
--- SpongePixelmonTesting/src/main/java/com/pixelmonseasonthree/mcdev/mcdev/SendMessage.java
+++ SpongePixelmonTesting/src/main/java/com/pixelmonseasonthree/mcdev/mcdev/SendMessage.java
@@ -8,11 +8,19 @@ import org.spongepowered.api.command.source.ConsoleSource;
 import org.spongepowered.api.command.spec.CommandExecutor;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.format.TextColors;
+import org.spongepowered.api.text.format.TextStyles;
 
 public class SendMessage implements CommandExecutor {
     @Override
     public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
             if(src instanceof Player){
+
+                Player player = (Player) src;
+                int x = player.getLocation().getBlockX(),
+                z = player.getLocation().getBlockZ();
+
+                player.sendMessage(Text.of(TextColors.AQUA,TextStyles.BOLD, "hello" + player.getName()));
 
 
             }
